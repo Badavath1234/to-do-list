73489f55e737a7efb1060d47aa327f7e8dc03086
BADAVATH VENKATESH <venkyit0206@gmail.comm>
2023-06-22 23:53:40 +0530

    working on delete & editing func

--- todoList.js
+++ todoList.js
@@ -1,24 +1,53 @@
-const inputField = document.querySelector('#text');
-const formButton = document.querySelector('#btnInput');
-const listItem = document.querySelector('#list-item');
+const inputField = document.querySelector("#text");
+const formButton = document.querySelector("#btnInput");
+const listItem = document.querySelector("#list-item");
 
+let arr = [];
 
+formButton.addEventListener("click", () => {
+  arr.push(inputField.value);
+  inputField.value = " ";
+  createElements();
+});
 
+function createElements() {
+  listItem.innerHTML = "";
+  arr.forEach((item, index) => {
+    const listCreate = document.createElement("li");
+    listCreate.innerHTML = item;
+    // console.log(li.innerHTML);
+    listItem.appendChild(listCreate);
+    console.log(listItem);
 
+    const editButton = document.createElement("button");
+    editButton.id = "editBtn";
+    editButton.innerHTML = "EDIT";
+    listCreate.appendChild(editButton);
+    editButton.addEventListener("click", () => {
+      editFun(index);
+    });
 
+    const deleteButton = document.createElement("button");
+    deleteButton.id = "deleteBtn";
+    deleteButton.innerHTML = "DELETE";
+    deleteButton.addEventListener("click", () => {
+      alert("hi hello");
+      deleteFun(index);
+    });
+    listCreate.appendChild(deleteButton);
+  });
+}
 
+function deleteFun(index) {
+  arr.splice(index, 1);
+  createElements();
+}
 
-const li =document.createElement('li');
-li.innerHTML = inputField.value;
-listItem.append(li);
+function editFun(index,listCreate) {
+  inputField.value = arr[index];
+  inputField.append(listCreate)
+  // listCreate.appendChild(listCreate)
+    createElements();
+}
 
-editButton = document.createElement('button');
-editButton.id = 'editBtn';
-editButton.innerHTML = 'EDIT';
-li.append(editButton);
-
-
-deleteButton =document.createElement('button');
-deleteButton.id ='deleteBtn';
-deleteButton.innerHTML = 'DELETE';
-li.append(deleteButton);
+console.log(arr);
